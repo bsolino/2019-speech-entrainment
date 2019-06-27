a3fed8fbca291d55f6549af6aefd65bacaac6107
Breixo Soliño <breixo.solino@gmail.com>
2019-06-27 02:43:09 +0200

    Implemented entrainment, tested latency of analysis: Quite fast!

--- src/game.py
+++ src/game.py
@@ -6,41 +6,93 @@ Created on Wed Jun  5 02:48:37 2019
 @author: breixo
 """
 
-import os
+from os import pardir, listdir, makedirs
+from os.path import join, abspath, exists
+from shutil import rmtree
 from nao_utils import create_proxy, IP, PORT
 #from word_list_utils import parse_file
 from task_utils import read_task_lists
+from audio_utils import record_manual, record_to_file
 from time import sleep
+from praat_utils import extract_features, parse_mean_pitch
+from distutils.util import strtobool
 
-FILENAME_TASKS = os.path.join("data", "tasks", "selected_tasks.txt")
+#CONSTANTS
+FILENAME_TASKS = join("data", "tasks", "selected_tasks.txt")
+PARTICIPANTS_DATA_FOLDER = abspath(join(pardir, "participant_data"))
 NAO_FOLDER = "/home/nao/entrainment"
 WORDS_FOLDER = NAO_FOLDER + "/words"
 INTERACTIONS_FOLDER = NAO_FOLDER + "/interactions_s100_p1.15"
 
+MIN_PITCH = 130
+MAX_PITCH = 350
+
+# CONFIGURATION
+participant_id = None
+entrainment = None
+p_folder = None
 
 
 # UTILS
 
-def find_word_file(word, words_folder):
+def find_word_file(word, words_folder, target_pitch):
     """
     Obtains the route to the word file from the word
     """
-    # TODO Improve to be able to select specific frequencies
-    return "{}/{}-base.wav".format(words_folder, word) # TODO Adjust 
+    if not entrainment:
+       target_pitch = ""
+    return "{}/{}-base{}.wav".format(words_folder, word, target_pitch)
+    
 
+
+
+def input_boolean(msg, default = False):
+    a_boolean = None
+    while a_boolean is None:
+        _input = raw_input(msg)
+        if _input:
+            try:
+                a_boolean = strtobool(_input)
+            except ValueError:
+                print("ERROR: {} is not a valid value.".format(_input))
+        else:
+            a_boolean = default
+    return a_boolean
+
+
+def analyze_audio_data(base_path, audio_data, sample_width):
+    audio_file = base_path + ".wav"
+    features_file = base_path + ".txt"
+    record_to_file(audio_file, audio_data, sample_width)
+    extract_features(audio_file, features_file)
+    return parse_mean_pitch(features_file)
+
+
+def round_and_bound_pitch(x):
+    base = 5
+    value = int(base * round(float(x)/base))
+    ret = max(MIN_PITCH, min(MAX_PITCH, value))
+    print("Original value = {}; Rounded = {}; returns = {}".format(x, value, ret))
+    return ret
 
 # ACTIONS
 
-def wait_for_kid(word = ""):
+def wait_for_kid(word = "", base_path = ""):
     if word:
         msg = "word: \"{}\"".format(word)
     else:
         msg = "utterance"
+    target_pitch = None
+    if base_path:
+        sample_width, audio_data = record_manual()
+        mean_pitch = analyze_audio_data(base_path, audio_data, sample_width)
+        target_pitch = round_and_bound_pitch(mean_pitch)
     raw_input("Waiting for {}. Press ENTER to continue".format(msg)) # Temporal solution
+    return target_pitch
 
 
-def play_file(filename, aup, folder):
-    word_file = find_word_file(filename, folder)
+def play_file(filename, target_freq, folder, aup):
+    word_file = find_word_file(filename, folder, target_freq)
     print(word_file)
     aup.post.playFile(word_file)
 
@@ -105,13 +157,18 @@ def speak_finish_experiment(aup):
 
 # TASKS
 
-def execute_task(task, aup, words_folder):
+def execute_task(task, i_task, aup, words_folder):
     """
     Plays the game: waits for kind input and then says translation
     """
-    for word in task.word_order:
-        wait_for_kid(word)
-        play_file(word, aup, words_folder)
+    n_words = len(task.word_order)
+    for i in range(n_words):
+        word = task.word_order[i]
+        results_name = "task_{:d}-word_{:0>2d}_{:s}".format(
+                i_task+1, i+1, word)
+        results_path = join(p_folder, results_name)
+        target_pitch = wait_for_kid(word, results_path)
+        play_file(word, target_pitch, words_folder, aup)
 
 
 def execute_tasks(list_tasks, aup, words_folder):
@@ -129,7 +186,7 @@ def execute_tasks(list_tasks, aup, words_folder):
 
         speak_before_task(aup, i)
         
-        execute_task(task, aup, words_folder)
+        execute_task(task, i, aup, words_folder)
         
         print("Finished task {}/{}".format(i+1, n_tasks))
         raw_input("Press ENTER to finish task")
@@ -137,16 +194,107 @@ def execute_tasks(list_tasks, aup, words_folder):
     speak_finish_experiment(aup)
 
 
+def get_last_participant_id(p_data_folder):
+    list_participants = listdir(p_data_folder)
+    list_participants.sort(reverse=True)
+    participant = None
+    for participant in list_participants:
+        if "id" in participant:
+            break
+    if participant:
+        last_id = participant.split("-")[0].split("_")[1]
+        return int(last_id)
+    else:
+        return 0
+    
+
+
+def find_participant_folder(participant_id, p_data_folder):
+    list_participants = listdir(p_data_folder)
+    id_code = "id_{:0>3d}".format(participant_id)
+    for participant in list_participants:
+        if id_code in participant:
+            return participant
+    return None
+
+
+def get_participant_id(p_data_folder):
+    last_id = get_last_participant_id(p_data_folder)
+    participant_id = None
+    while not participant_id:
+        participant_input = raw_input(
+                "Participant ID? - Empty = {:>2d}: ".format(last_id + 1))
+        if participant_input:
+            try:
+                participant_id = int(participant_input)
+            except ValueError:
+                print("ERROR: {} is not a valid integer".format(
+                        participant_input))
+        else:
+            participant_id = last_id +1 
+    print("Participant {:>2d}".format(participant_id))
+    p_folder = find_participant_folder(
+            participant_id, p_data_folder)
+    if p_folder:
+        if input_boolean("A folder for participant id {:0>3d} ".format(
+                                 participant_id)
+                         + "already exists: \"{}\"".format(p_folder)
+                         + ", delete? (y/N) - Empty = NO: ",
+                         False):
+            rmtree(abspath(join(p_data_folder, p_folder)))
+        else:
+            return get_participant_id(p_data_folder)
+    return participant_id
+
+
+
+def get_entrainment_condition():
+    entrainment = input_boolean(
+            "Entrainment condition (y/N) - Empty = NO: ", False)
+    print("Entrainment condition: {}".format(bool(entrainment)))
+    return entrainment
+
+
+def set_configuration(p_data_folder):
+    
+    global participant_id
+    participant_id = get_participant_id(p_data_folder)
+    
+    global entrainment
+    entrainment = get_entrainment_condition()
+    
+    global p_folder
+    p_folder = abspath(join(p_data_folder,"id_{:0>3d}-entrainment_{}".format(
+            participant_id, entrainment)))
+    makedirs(p_folder) # At this point it shouldn't exist
+
+
 def main():
     # Parse properties
     f_tasks = FILENAME_TASKS
+    p_data_folder = PARTICIPANTS_DATA_FOLDER
     words_folder = WORDS_FOLDER
-    interactions_folder = INTERACTIONS_FOLDER
+#    interactions_folder = INTERACTIONS_FOLDER
     ip = IP
     port = PORT
-
-    aup = create_proxy("ALAudioPlayer", ip, port)
     
+    if not exists(p_data_folder):
+        makedirs(p_data_folder)
+    
+    set_configuration(p_data_folder)
+
+#    aup = create_proxy("ALAudioPlayer", ip, port)
+    
+    class MockAup:
+        class post:
+            @staticmethod
+            def playFile(path):
+                print("Non blocking. File: {}".format(path))
+        
+        @staticmethod
+        def playFile(path):
+            print("Blocking. File: {}".format(path))
+    #aup = MockAup
     
     list_tasks = read_task_lists(f_tasks)
     execute_tasks(list_tasks, aup, words_folder)
--- src/tests/praat_integration_test.py
+++ src/tests/praat_integration_test.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-#import os
+import os
 
 import praat_utils
+from datetime import datetime
+from audio_utils import record_manual
+from game import analyze_audio_data
+
+
+def call_praat_test():
+    sound_file = os.path.join("return", "test", "demo.wav")
+    return_file = os.path.abspath("return/tmp/extract_features.tmp")
+#    praat_utils.extract_features(sound_file, return_file)
+    praat_utils.extract_features(sound_file, return_file)
+    print(praat_utils.parse_mean_pitch(return_file))
+
+
+def time_analyze_recording():
+    base_path = os.path.join("return", "test", "test_analyze_time")
+    
+    sample_width, audio_data = record_manual()
+    start = datetime.now()
+    mean_pitch = analyze_audio_data(base_path, audio_data, sample_width)
+    end = datetime.now()
+    
+    print("Time: {} - Mean pitch: {}".format(
+            str(end - start), mean_pitch))
+
 
 def main():
-    sound_file = "demo.wav"
-#    return_file = os.path.abspath("return/dance-base.txt")
-#    praat_utils.extract_features(sound_file, return_file)
-    print(praat_utils.extract_features(sound_file))
+#    call_praat_test()
+    time_analyze_recording()
 
 
 if __name__ == "__main__":
