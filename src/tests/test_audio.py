08b957f047b435a7eb947aac4d18f96f74956fc3
Breixo Soliño <breixo.solino@gmail.com>
2019-06-27 02:44:09 +0200

    Updated test paths

--- src/tests/test_audio.py
+++ src/tests/test_audio.py
@@ -7,18 +7,21 @@ Created on Mon Jun 24 23:55:16 2019
 """
 
 from audio_utils import record_manual, record_automatic, record_to_file
+from os.path import join
 
 
 def demo_automatic():
     print("Wait in silence to begin recording; wait in silence to terminate")
     sample_width, data = record_automatic()
-    record_to_file('demo.wav', data, sample_width)
+    record_to_file(join("return", "test", "demo.wav"), data, sample_width)
     print("done - result written to demo.wav")
 
 
 def demo_manual():
     sample_width, data = record_manual()
-    record_to_file('demo.wav', data, sample_width)
+    
+    
+    record_to_file(join("return", "test", "demo.wav"), data, sample_width)
     print("done - result written to demo.wav")
 
 def main():
