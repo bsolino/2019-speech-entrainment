a442e09b75aa3b2b2c5ee5ff50d59c0ee6d31a53
Breixo Soliño <breixo.solino@gmail.com>
2019-06-27 18:00:01 +0200

    Added adapt

--- src/praat_utils.py
+++ src/praat_utils.py
@@ -9,8 +9,8 @@ Created on Tue Jun 18 07:29:25 2019
 from os.path import join, abspath
 from subprocess import call
 
-PRAAT_SCRIPT_PATH = abspath(join("..", "misc"))
-EXTRACT_PRAAT_SCRIPT = join(PRAAT_SCRIPT_PATH, "extract_features.praat")
+PRAAT_SCRIPT_FOLDER = abspath(join("..", "misc"))
+EXTRACT_PRAAT_SCRIPT = join(PRAAT_SCRIPT_FOLDER, "extract_features.praat")
 """
     sentence filename
     sentence outfilename
@@ -20,7 +20,11 @@ EXTRACT_PRAAT_SCRIPT = join(PRAAT_SCRIPT_PATH, "extract_features.praat")
     boolean extract_jitter_shimmer 1
 
 """
-SCRIPT_PITCH_OPTION = ["0", "1", "0", "0"]
+EXTRACT_SCRIPT_PITCH_OPTION = ["0", "1", "0", "0"]
+
+ADAPT_PRAAT_SCRIPT = join(join(PRAAT_SCRIPT_FOLDER, "adapt.praat"))
+
+
 PRAAT_LOCATION = "praat"
 
 def call_praat(script, args = None):
@@ -39,8 +43,24 @@ def call_praat(script, args = None):
     assert (result == 0)
 
 
-def adapt():
-    pass # TODO
+def adapt(in_sound, out_sound, target_mean_pitch):
+    """
+    sentence in_fname
+    sentence out_fname
+    integer syllable_count
+    real target_rate_syll_per_s
+    real target_intensity_mean_db
+    real target_pitch_mean_hz
+"""
+    args = [
+            in_sound,
+            out_sound,
+            "-1",
+            "-1",
+            "-1",
+            str(target_mean_pitch)
+            ]
+    call_praat(ADAPT_PRAAT_SCRIPT, args)
 
 
 def parse_feature_line(line):
@@ -78,9 +98,9 @@ def extract_features(sound_file, return_file):
 #            EXTRACT_PRAAT_SCRIPT,
 #            sound_file,
 #            return_file
-#            ] + SCRIPT_PITCH_OPTION
+#            ] + EXTRACT_SCRIPT_PITCH_OPTION
 #    result = call(praat_call)
 #    assert (result == 0)  # Check that the script worked
-    args = [sound_file, return_file] + SCRIPT_PITCH_OPTION
+    args = [sound_file, return_file] + EXTRACT_SCRIPT_PITCH_OPTION
     call_praat(EXTRACT_PRAAT_SCRIPT, args)
 
