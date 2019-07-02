81aa8b81d19312badbdfa594aa790cefb4a310bd
Breixo Soliño <breixo.solino@gmail.com>
2019-07-02 16:48:12 +0200

    Hack to avoid ALSA Warnings

---
+++ src/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  2 13:01:19 2019
+
+@author: breixo
+"""
+
+"""
+Hack to Silence PyAudio warnings
+"""
+
+import sys
+from os.path import join
+sys.stderr = open(join("return","logfile.txt"), 'wb')
+
+execfile('game.py')
